46fdf24177ab4c5fc418641645a81be6c3ac7ee4
Mohammed Yousuf <Mohammed Yousuf@LAPTOP-0IRDCJ3H>
2022-02-10 06:49:14 +0530

    Valid and invalid names for variables

---
+++ Hello_world/src/variable3.java
@@ -0,0 +1,34 @@
+
+public class variable3 {
+
+	public static void main(String[] args) {
+		//Valid Names or identifers for a variable
+		
+		int weight;
+		int _weight;
+		int $weight;
+		int wei3ght;
+		int w_ei4_3ght;
+		int weight2;
+		
+		//Invalid Names or identifers for a variables 
+		
+		//int class;//invalid - u cannot have key words as identifiers
+		//int 2weight;//invalid - u cannot start a variable name with a number
+		//int &weight;//invalid - no special character other than _ and $ can be used
+		//int we ight;//invalid - you cannot have space
+				
+		//Note: Remember always, the method names should start with lower case
+		//variable names should also start with lower case
+		//package names should also start with lower caser
+		//class names should always start with upper case
+		//and the next word should always start with upper case in all
+				
+		int distanceToMoon;//toggle case or camel case
+		//class EmployeeDemo
+		
+		
+
+	}
+
+}
